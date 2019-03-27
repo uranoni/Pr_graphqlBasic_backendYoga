3636d72ebcf0c82bcfd5845ea400b3c5bb2a3bec
roni <uranoni777@gmail.com>
2019-03-27 14:13:32 +0800

    arguments explain

--- src/index.js
+++ src/index.js
@@ -5,30 +5,43 @@ import { GraphQLServer } from 'graphql-yoga'
 //reference it by its name  User(Query)  in custom type (User) 
 // custom type cab be use nullable when you decide
 const typeDefs = `
-  type Query {
-        me:User!
-        post:Post!
+    type Query {
+        greeting(name: String, position: String): String!
+        add(a: Float!, b: Float!): Float!
+        me: User!
+        post: Post!
     }
 
-    type User{
-        id:ID!
-        name:String!
-        email:String!
-        age:Int
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
     }
 
-    type Post{
-        id:ID!
-        title:String!
-        body:String!
-        published:Boolean!
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
 // Resolvers 
 const resolvers = {
     Query: {
-        // match up me return object to user
-        // in this case resolver not a Scalar va;ue use curly bracket
+        // parents is root query 
+        //args is client data across 
+        //ctx 
+        greeting(parent, args, ctx, info) {
+            if (args.name && args.position) {
+                return `Hello, ${args.name}! You are my favoriate ${args.position}.`
+            } else {
+                return 'Hello!'
+            }
+        },
+        add(parent, args, ctx, info) {
+            return args.a + args.b
+        },
         me: () => {
             return {
                 id: "M0724001",
