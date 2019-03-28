7160f7cf027b7379deea0cbd0f764376249731eb
uranoni <uranoni777@gmail.com>
2019-03-29 04:22:11 +0800

    add posts query and schema

--- src/index.js
+++ src/index.js
@@ -18,9 +18,27 @@ const users = [
         email:"mike@gmail.com"
     }
 ]
+
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: true
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: false
+}]
 const typeDefs = `
     type Query {
         users(query:String):[User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
@@ -49,6 +67,17 @@ const resolvers = {
             }
             return users.filter((user)=>{
                 return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
             })
         },
         me: () => {
