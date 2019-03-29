2f57858f75ca58adbf246a19f1d9316ad6097157
uranoni <uranoni777@gmail.com>
2019-03-29 10:22:25 +0800

    associate each post to author data

--- src/index.js
+++ src/index.js
@@ -2,39 +2,45 @@ import { GraphQLServer } from 'graphql-yoga'
 
 const users = [
     {
-        id:'1',
-        name:'Roni',
-        email:"roni@gmail.com",
-        age:'24'
+        id: '1',
+        name: 'Roni',
+        email: "roni@gmail.com",
+        age: '24'
     },
     {
-        id:'2',
-        name:"Kao",
-        email:"Kao@gmail.com"
+        id: '2',
+        name: "Kao",
+        email: "Kao@gmail.com"
     },
     {
-        id:'3',
-        name:"Mike",
-        email:"mike@gmail.com"
+        id: '3',
+        name: "Mike",
+        email: "mike@gmail.com"
     }
 ]
 
+//make post data accosicate author
 const posts = [{
     id: '10',
     title: 'GraphQL 101',
     body: 'This is how to use GraphQL...',
-    published: true
+    published: true,
+    author: '1'
 }, {
     id: '11',
     title: 'GraphQL 201',
     body: 'This is an advanced GraphQL post...',
-    published: false
+    published: false,
+    author: '1'
 }, {
     id: '12',
     title: 'Programming Music',
     body: '',
-    published: false
+    published: false,
+    author: '2'
 }]
+
+// setting up a filed whose value is anoter custom type
 const typeDefs = `
     type Query {
         users(query:String):[User!]!
@@ -55,17 +61,18 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
     }
 `
 // Resolvers 
 const resolvers = {
     Query: {
         // each object match schema type
-        users(parent,args,ctx,info){
-            if(!args.query){
+        users(parent, args, ctx, info) {
+            if (!args.query) {
                 return users
             }
-            return users.filter((user)=>{
+            return users.filter((user) => {
                 return user.name.toLowerCase().includes(args.query.toLowerCase())
             })
         },
@@ -95,6 +102,15 @@ const resolvers = {
                 body: "test GraphQL",
                 published: true
             }
+        }
+    },
+    // set up property match post 
+    // and post information live  in parent args
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
         }
     }
 }
