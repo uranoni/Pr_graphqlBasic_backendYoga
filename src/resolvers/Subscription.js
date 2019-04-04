e7ac5da996fcb60f1ba902bef074fc4061372e42
uranoni <uranoni777@gmail.com>
2019-04-04 21:20:44 +0800

    comment subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -11,6 +11,18 @@ const Subscription ={
 			},1000)
 			return pubsub.asyncIterator('count')
 		}
+	},
+	comment:{
+		subscribe(parent,{postId},{db,pubsub},info){
+			const post = db.posts.find((post)=>post.id === postId && post.published)
+
+			if(!post){
+				throw new Error("Post not found ")
+			}
+
+			return pubsub.asyncIterator(`comment ${postId}`)
+
+		}
 	}
 }
 
