e5be2eea12e124bcb87ea8f98be00130206e5de1
uranoni <uranoni777@gmail.com>
2019-04-04 20:33:07 +0800

    compelete mutation

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -1,4 +1,5 @@
 import uuidv4 from 'uuid/v4'
+import { type } from 'os';
 const Mutation = {
   createUser(parent,args,{db},info){
       const emailTaken = db.users.some( (user)=>  user.email === args.data.email )
@@ -38,6 +39,34 @@ const Mutation = {
 
       return deleUsers[0]
   },
+  updateUser(parent,args,{db},info){
+        const {id,data} = args
+        const user = db.users.find((user)=>user.id === id)
+
+        if(!user){
+            throw new Error("User not found")
+        }
+
+        if(typeof data.email === "string"){
+            const emailTaken = db.users.some((user)=>user.email === data.email)
+
+            if(emailTaken){
+                throw new Error('Email taken')
+            }
+
+            user.email = data.email
+        }
+
+        if(typeof data.name === 'string'){
+            user.name = data.name
+        }
+
+        if(typeof data.age !== 'undefined'){
+            user.age = data.age
+        }
+
+        return user
+  },
   createPost(parent,args,{db},info){
       const userExists = db.users.some((user) => {
 
@@ -72,6 +101,28 @@ const Mutation = {
       db.comments = db.comments.filter((comment)=> comment.post !== args.id)
       return deletedPosts[0]
   },
+  updatePost(parent,args,{db},info){
+      const {id, data}=args
+      const post = db.posts.find((post) => post.id === id)
+
+      if(!post){
+          throw new Error("unable to find user and post")
+      }
+
+      if(typeof data.title === 'string'){
+          post.title =data.title
+      }
+
+      if(typeof data.body === 'string'){
+          post.body = data.body
+      }
+
+      if(typeof data.published === 'boolean'){
+          post.published = data.published
+      }
+
+      return post
+  },
   // return post.id === args.post && post.published === true
   createComment(parent,args,{db},info){
       const userExists = db.users.some((user)=> user.id === args.data.author)
@@ -103,6 +154,20 @@ const Mutation = {
       const deleteComments = db.comments.splice(commentIndex,1)
 
       return deleteComments[0]
+  },
+  updateComment(parent,args,{db},info){
+      const {id,data} = args
+      const comment = db.comments.find((comment)=> comment.id === id )
+
+      if(!comment){
+          throw new Error("comment not found")
+      }
+
+      if(typeof data.text === 'string'){
+          comment.text = data.text
+      }
+
+      return comment
   }
 }
 
