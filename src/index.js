6ec09b5d41f7e8ba7af838a8eeb3729d90e3e89c
uranoni <uranoni777@gmail.com>
2019-04-04 21:01:35 +0800

    subscription test

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import uuidv4 from 'uuid/v4'
 import { composeTransforms } from 'graphql-tools/dist/transforms/transforms';
 import db from './db'
 import Query from "./resolvers/Query";
 import  Mutation  from "./resolvers/Mutations";
+import Subscription from './resolvers/Subscription'
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment"
 
+const pubsub = new PubSub()
+
 const server = new GraphQLServer({
     typeDefs:'./src/schema.graphql', 
     resolvers:{
-        Query,Mutation,User,Post,Comment
+        Query,Mutation,User,Post,Comment,Subscription
     },
     context:{
-        db
+        db,pubsub
     }
 })
 
